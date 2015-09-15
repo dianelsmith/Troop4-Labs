8fda9d22dd611cb670e50211ecb9bc8406bc766a
Diane L Smith <dianelsmith@cox.net>
2015-09-14 20:24:00 -0400

    Day21 EmployeeApp

---
+++ Day21/Day21/ngApps/EmployeeApp/employeeApp.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+
+namespace EmployeeApp {
+
+    angular.module('EmployeeApp', []);
+
+}
---
+++ Day21/Day21/ngApps/EmployeeApp/employeeControllers.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+
+
+namespace EmployeeApp.Controllers {
+
+    class Employee {
+        firstName: string;
+        lastName: string;
+        age: number;
+        employeeType: string;
+        yearlySalary = 0;
+        hourlySalary = 0;
+    }
+
+    class EmployeeController {
+        employee: Employee;
+
+        save() {
+            if (this.employee.employeeType === 'FullTime') {
+                alert(`Saved new ${this.employee.employeeType} Employee ${this.employee.firstName} ${this.employee.lastName} age ${this.employee.age} with Yearly Salary of $${this.employee.yearlySalary}`);
+            } else {
+                alert(`Saved new ${this.employee.employeeType} Employee ${this.employee.firstName} ${this.employee.lastName} age ${this.employee.age} with Hourly Salary of $${this.employee.hourlySalary}`);
+            }
+
+
+        }
+
+    }
+
+    angular.module('EmployeeApp').controller("EmployeeController", EmployeeController);
+
+} 
